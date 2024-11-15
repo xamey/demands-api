20d253e7f71e6aba1b6e32643735cee0beab8811
xamey <ecallemax@gmail.com>
2024-11-15 22:45:27 +0100

    Modified seed to add dayoffs

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,5 +1,7 @@
-import { users } from "./schema";
+import { dayoffs, users } from "./schema";
 import db from "./connection";
+import { eq } from "drizzle-orm";
+import dayjs from "dayjs";
 
 const main = async () => {
   await db.delete(users);
@@ -21,6 +23,32 @@ const main = async () => {
       email: "test2@test.com",
       password: Bun.password.hashSync("test"),
       superUser: false,
+    },
+  ]);
+
+  const test1 = (
+    await db.select().from(users).where(eq(users.email, "test1@test.com"))
+  )[0];
+
+  const test2 = (
+    await db.select().from(users).where(eq(users.email, "test2@test.com"))
+  )[0];
+
+  await db.insert(dayoffs).values([
+    {
+      userId: test1.id,
+      date: dayjs().toISOString(),
+      status: "pending",
+    },
+    {
+      userId: test1.id,
+      date: dayjs().subtract(1, "day").toISOString(),
+      status: "approved",
+    },
+    {
+      userId: test2.id,
+      date: dayjs().toISOString(),
+      status: "pending",
     },
   ]);
 };
